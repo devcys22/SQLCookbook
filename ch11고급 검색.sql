54fe613343ff339652ac64ffbad0ed4357b35c35
dev22 <78769412+devcys22@users.noreply.github.com>
2023-04-10 07:24:19 +0900

    Update ch11고급 검색.sql

--- ch11고급 검색.sql
+++ ch11고급 검색.sql
@@ -29,3 +29,22 @@ select sal
        )
        )
  where rn between 6 and 10
+
+11.2 테이블에서 n개 행 건너뛰기
+  select ename
+    from (
+  select row_number() over (order by ename) rn,
+         ename
+    from emp
+         ) x
+   where mod(rn,2) = 1
+--------------------------------------------------------------------
+select row_number() over (order by ename) rn, ename
+  from emp
+
+11.3 외부 조인을 사용할 때 OR 로직 통합하기
+select e.ename, d.deptno, d.dname, d.loc
+  from dept d, emp e
+ where d.deptno = e.deptno
+   and (e.deptno = 10 or e.deptno = 20)
+ order by 2
