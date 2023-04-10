964ae8ab009ce31032d07de591f86e83fc09090e
dev22 <78769412+devcys22@users.noreply.github.com>
2023-04-11 06:50:52 +0900

    ✨ 11.5 상위 n개 레코드 선택하기

--- ch11고급 검색.sql
+++ ch11고급 검색.sql
@@ -48,3 +48,54 @@ select e.ename, d.deptno, d.dname, d.loc
  where d.deptno = e.deptno
    and (e.deptno = 10 or e.deptno = 20)
  order by 2
+ --------------------------------------------------------------------
+select e.ename, d.deptno, d.dname, d.loc
+  from dept d left join emp e
+    on (d.deptno = e.deptno)
+ where e.deptno = 10
+    or e.deptno = 20
+ order by 2
+--------------------------------------------------------------------
+  select e.ename, d.deptno, d.dname, d.loc
+    from dept d left join emp e
+      on (d.deptno = e.deptno
+         and (e.deptno=10 or e.deptno=20))
+   order by 2
+--------------------------------------------------------------------
+  select e.ename, d.deptno, d.dname, d.loc
+    from dept d
+    left join
+         (select ename, deptno
+            from emp
+           where deptno in ( 10, 20 )
+         ) e on ( e.deptno = d.deptno )
+  order by 2
+
+11.4 역수 행 확인하기
+select *
+  from V
+--------------------------------------------------------------------
+select distinct v1.*
+  from V v1, V v2
+ where v1.test1 = v2.test2
+   and v1.test2 = v2.test1
+   and v1.test1 <= v1.test2
+--------------------------------------------------------------------
+select v1.*
+  from V v1, V v2
+ where v1.test1 = v2.test2
+   and v1.test2 = v2.test1
+
+11.5 상위 n개 레코드 선택하기
+  select ename,sal
+   from (
+  select ename, sal,
+         dense_rank() over (order by sal desc) dr
+    from emp
+         ) x
+   where dr <= 5
+--------------------------------------------------------------------
+select ename, sal,
+       dense_rank() over (order by sal desc) dr
+  from emp
+  
