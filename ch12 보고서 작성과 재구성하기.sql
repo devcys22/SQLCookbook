cb90fbc8a5f99d7bedb65c73901f5332740eb08e
dev22 <78769412+devcys22@users.noreply.github.com>
2023-04-19 06:59:47 +0900

    ✨ feature: 12.10. 수직 히스토그램 생성하기

--- ch12 보고서 작성과 재구성하기.sql
+++ ch12 보고서 작성과 재구성하기.sql
@@ -186,3 +186,36 @@ select row_number()over(order by empno) rn,
         lpad('*',count(*),'*') as cnt
    from emp
   group by deptno
+
+12.10. 수직 히스토그램 생성하기
+ select max(deptno_10) d10,
+         max(deptno_20) d20,
+         max(deptno_30) d30
+    from (
+  select row_number()over(partition by deptno order by empno) rn,
+         case when deptno=10 then '*' else null end deptno_10,
+         case when deptno=20 then '*' else null end deptno_20,
+         case when deptno=30 then '*' else null end deptno_30
+    from emp
+        ) x
+  group by rn
+  order by 1 desc, 2 desc, 3 desc
+--------------------------------------------------------------------
+select row_number()over(partition by deptno order by empno) rn,
+       case when deptno=10 then '*' else null end deptno_10,
+       case when deptno=20 then '*' else null end deptno_20,
+       case when deptno=30 then '*' else null end deptno_30
+  from emp
+--------------------------------------------------------------------
+select max(deptno_10) d10,
+       max(deptno_20) d20,
+       max(deptno_30) d30
+  from (
+select row_number()over(partition by deptno order by empno) rn,
+       case when deptno=10 then '*' else null end deptno_10,
+       case when deptno=20 then '*' else null end deptno_20,
+       case when deptno=30 then '*' else null end deptno_30
+  from emp
+       ) x
+ group by rn
+ order by 1 desc, 2 desc, 3 desc
